50179bbe49cfa9ba860757704fe4e1c2d5dec59c
Alexei Abianna <alexeiabianna@gmail.com>
2020-09-16 22:36:57 -0300

    First commit

---
+++ Produto.java
@@ -0,0 +1,21 @@
+public class Produto {
+    private int codigo;
+    private String descricao;
+    private float preco;
+
+    public Produto(int c){
+        ...
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String Descricao() {
+        return descricao;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+}
