8dfd513d79e75eb49f89aa4a7d58b2fded5fb001
Alexei Abianna <alexeiabianna@gmail.com>
2020-09-16 22:35:57 -0300

    First commit

---
+++ Estoque.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Estoque {
+   private ArrayList<ItemDeEstoque> estoque;
+
+   public Estoque() {
+       estoque = new ArrayList<>(100);
+   }
+
+   public void chagadaDeProd(){
+       
+   }
+}
